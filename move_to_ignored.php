0e72abf90e926146bb1d78a0eb688e0fc9e903b1
manjulagunarathna <manjulagunarathna500@gmail.com>
2025-02-22 13:33:56 +0530

    Added the scripts for moving applications

--- move_to_ignored.php
+++ move_to_ignored.php
@@ -1,9 +1,13 @@
 <?php
 require 'db_connect.php';
-$id = $_GET['id'];
-$sql = "INSERT INTO ignored_applicants SELECT * FROM applicants WHERE id = $id";
-$conn->query($sql);
-$conn->query("DELETE FROM applicants WHERE id = $id");
-header("Location: admin_dashboard.php");
-exit();
+if (isset($_GET['id'])) {
+    $id = intval($_GET['id']);
+    $stmt = $conn->prepare("INSERT INTO ignored_applicants SELECT * FROM applicants WHERE id = ?");
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+    $stmt = $conn->prepare("DELETE FROM applicants WHERE id = ?");
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+    header("Location: admin_dashboard.php");
+}
 ?>
